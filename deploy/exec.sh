e46d9e7a5c20346fd18f2d32906e373ec2a0667a
hizzuu <52273972+hizzuu@users.noreply.github.com>
2022-08-09 13:41:36 +0900

    add exec.sh

--- deploy/exec.sh
+++ deploy/exec.sh
@@ -8,7 +8,9 @@ FLG=$3
 
 LOGFILE=app.log
 
-rm ${LOGFILE}
+if [ -e LOGFILE ]; then
+  rm ${LOGFILE}
+fi
 
 # バイナリ実行
 ./code-pipeline-example -name="${NAME}" -num="${NUM}" -flg="${FLG}" > ${LOGFILE} 2>&1
